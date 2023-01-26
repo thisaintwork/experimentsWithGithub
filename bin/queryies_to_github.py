f9713a5064df823aa5c9222f26b55ccb681b74b6
mreekie <mreekie@iq.harvard.edu>
2023-01-26 07:36:45 -0500

    back to  using ths file for the project experimentation. Working on getting project view information

---
+++ bin/queryies_to_github.py
@@ -0,0 +1,67 @@
+# An example to get the remaining rate limit using the Github GraphQL API.
+# https://gist.github.com/gbaman/b3137e18c739e0cf98539bf4ec4366ad#file-graphql_example-py
+
+import requests
+import os
+
+filename = '/home/perftest/DevCode/github-com-mreekie/GitHubProjects/experimentsWithGithub/input/testquery-01.graphql'
+# with open(filename, 'r') as file:
+# query =  file.read()
+
+# file.close()
+
+# query = '"""' + query + '"""' + "\n"
+
+#print("input - {}".format(query))
+
+key = 'AUTH_TKN'
+value = os.getenv(key, "novalue")
+# print("headers {} - {}".format(key, value))
+
+headers = {'Authorization': 'Bearer ' + value}
+
+
+# print("headers - {}".format(headers))
+
+
+# A simple function to use requests.post to make the API call. Note the json= section.
+def run_query(querystr):
+    request = requests.post('https://api.github.com/graphql', json={'query': querystr}, headers=headers)
+
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+
+# The GraphQL query (with a few additional bits included) itself defined as a multi-line string.
+query1 = """
+{
+organization(login: "IQSS") {
+        projectV2 (number: 34) {
+            title
+            closed
+            number
+            createdAt
+            updatedAt
+            closedAt
+            url
+            creator {
+                login
+            }
+            views {
+                totalCount
+            }
+        }
+        id
+        email
+        projectsUrl
+        login
+    }
+
+}
+"""
+
+result = run_query(query1)  # Execute the query
+# orgnzn = result["github"]["organization"]
+print("output - {}".format(result))
