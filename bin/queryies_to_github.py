de2914f14940cf1ac843bc559b975302b3c4ec9a
mreekie <mreekie@iq.harvard.edu>
2023-02-03 10:47:17 -0500

    #12 I got the same query working again, this time with graphql. Still needs some cleanup.

--- bin/queryies_to_github.py
+++ bin/queryies_to_github.py
@@ -1,44 +1,55 @@
 # An example to get the remaining rate limit using the Github GraphQL API.
-# https://gist.github.com/gbaman/b3137e18c739e0cf98539bf4ec4366ad#file-graphql_example-py
 
-import requests
-import os
-import json
-
-# import the graphql library for python
 from gql import gql, Client
-from gql.transport.requests import RequestsHTTPTransport
-
-
-filename = '/home/perftest/DevCode/github-com-mreekie/GitHubProjects/experimentsWithGithub/input/testquery-01.graphql'
-# with open(filename, 'r') as file:
-# query =  file.read()
-
-# file.close()
-
-# query = '"""' + query + '"""' + "\n"
-
-#print("input - {}".format(query))
+from gql.transport.aiohttp import AIOHTTPTransport
+import os
 
 key = 'AUTH_TKN'
 value = os.getenv(key, "novalue")
-# print("headers {} - {}".format(key, value))
 
-headers = {'Authorization': 'Bearer ' + value}
+headers = {"Authorization": "Bearer " + value}
+transport = AIOHTTPTransport(url='https://api.github.com/graphql', headers=headers)
+client = Client(transport=transport)
+
+print("headers {} - {}".format(key, value))
+print("headers {}".format(headers))
+
+
+
+query_filename = '../input/input_query.graphql'
+# read graphql query from file
+
+def load_query(query_filename):
+    with open(query_filename) as file:
+        return gql(file.read())
+
+
+
+query_string = load_query(query_filename)
+response = client.execute(query_string)
+print(response)
+
+
+
+# query = '"""' + query + '"""' + "\n"
+
+# print("input - {}".format(query))
+
 
 
 # print("headers - {}".format(headers))
 
 
 # A simple function to use requests.post to make the API call. Note the json= section.
-def run_query(querystr):
-    request = requests.post('https://api.github.com/graphql', json={'query': querystr}, headers=headers)
-
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
+#def run_query(querystr):
+#    request = requests.post('https://api.github.com/graphql', json={'query': querystr}, headers=headers)
+#
+#
+#    if request.status_code == 200:
+#        return request.json()
+#    else:
+#        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, querystr))
+#
 
 # The GraphQL query (with a few additional bits included) itself defined as a multi-line string.
 query1 = """
@@ -164,11 +175,10 @@ organization(login: "IQSS") {
 }
 """
 
-parsed_json = run_query(query1)  # Execute the query
-#parsed_json = json.loads(ugly_json)
-pretty_json = json.dumps(parsed_json, indent=4)
-print(pretty_json)
-
+#parsed_json = run_query(query_string)  # Execute the query
+# parsed_json = json.loads(ugly_json)
+#pretty_json = json.dumps(parsed_json, indent=4)
+#print(pretty_json)
 
 # orgnzn = result["github"]["organization"]
-#print("output - {}".format(result))
+# print("output - {}".format(result))
