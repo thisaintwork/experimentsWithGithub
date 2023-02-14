e0a43609e23758822d56b4e40a3b1671302d3637
mreekie <mreekie@iq.harvard.edu>
2023-02-14 09:56:02 -0500

    #12 I believe that I have the first loop using pagination complete. Turns out not to be hard. Still need to verify correctness using output.txt. next step is to parse out the issue information. added project id as variable to queryies_to_github.py

--- bin/queryies_to_github.py
+++ bin/queryies_to_github.py
@@ -1,32 +1,63 @@
-# An example to get the remaining rate limit using the Github GraphQL API.
+# An example to get the remaining rate limit using the GitHub GraphQL API.
 
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 import os
 import json
 
-key = 'AUTH_TKN'
-value = os.getenv(key, "novalue")
 
-headers = {"Authorization": "Bearer " + value}
-transport = AIOHTTPTransport(url='https://api.github.com/graphql', headers=headers)
-client = Client(transport=transport)
-
-query_filename = '../input/input_query.graphql'
-
-
-# read graphql query from file
-
-def load_query(query_filename):
-    with open(query_filename) as file:
+def load_query(query_file):
+    with open(query_file) as file:
         return gql(file.read())
 
 
-query_string = load_query(query_filename)
-page_size = 2
-varsit = {"first": page_size}
-response = client.execute(query_string, variable_values=varsit)
-print("{}".format(response))
-print("output - {}".format(response['organization']['projectV2']['items']['totalCount']))
+# credit: https://til.simonwillison.net/github/graphql-pagination-python
+# Get all the items
+def get_all_items(auth_token_val, query_str, vars_in):
+    has_next_page = True
+    start_with = None
+    query_input_params = vars_in
+    print("query_input_params \"{}\"".format(query_input_params))
+
+    headers = {"Authorization": "Bearer " + auth_token_val}
+    transport = AIOHTTPTransport(url='https://api.github.com/graphql', headers=headers)
+    client = Client(transport=transport)
+
+    while has_next_page:
+        data = client.execute(query_str, variable_values=query_input_params)
+        # print results
+        print("totalCount \"{}\"".format(data['organization']['projectV2']['items']['totalCount']))
+        print("hasNextPage \"{}\"".format(data['organization']['projectV2']['items']['pageInfo']['hasNextPage']))
+        print("startCursor \"{}\"".format(data['organization']['projectV2']['items']['pageInfo']['startCursor']))
+        print("endCursor - \"{}\"".format(data['organization']['projectV2']['items']['pageInfo']['endCursor']))
+        #print("endCursor - \"{}\"".format(data['organization']['projectV2']['items']['nodes']['content']))
+        #print("returned results \"{}\"".format(data))
+
+        print()
+        print(json.dumps(data, indent=4))
+        print()
+        has_next_page = data['organization']['projectV2']['items']['pageInfo']['hasNextPage']
+        start_with = data['organization']['projectV2']['items']['pageInfo']['endCursor']
+        query_input_params["startWith"] = start_with
+        print("query_input_params \"{}\"".format(query_input_params))
+
+    return
 
 
+################################################
+# get OAUTH token
+key = 'AUTH_TKN'
+authTokenVal = os.getenv(key, "novalue")
+
+################################################
+# read graphql query from file
+query_filename = '../input/input_query.graphql'
+queryString = load_query(query_filename)
+
+################################################
+# Setup the query variables
+varsIn = {"loginOrg": "IQSS", "firstFew": 10, "projectNum": 34}
+
+################################################
+# Get all the data
+get_all_items(authTokenVal, queryString, varsIn)
