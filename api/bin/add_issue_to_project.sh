02f37ffc4be3a13a5867bb92ffe55f9558124bb2
mreekie <mreekie@iq.harvard.edu>
2023-02-28 18:57:35 -0500

    tweaked and re-tested add_issue_to_project.sh

--- api/bin/add_issue_to_project.sh
+++ api/bin/add_issue_to_project.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/bash
 set -o errexit
 
-cat<<EOF
-#===========================
-# Begin: $0
-EOF
-
-echo "# Set basedir to current dir of this script"
+# ###########################################################
+# Initialize the environment and global variables.
+# -----------------------------------------------------------
 BASEDIR=$(dirname "$0")
+
+echo "# import the environment variables"
+echo
+. environment.sh
+
 RELINPUTDIR=../input
 RELOUTPUTDIR=../output
 RELWRKDIR=../wrk
 RELBINDIR=../bin
 
-. environment.sh
+# ###########################################################
+# LOCAL VARIABLE CUSTOMIZATION
+# -----------------------------------------------------------
 
 # override the RUNDIR here
 #RUNDIR=.
 RUNINPUTDIR=${RUNDIR}/input
 RUNOUTPUTDIR=${RUNDIR}/output
 RUNWRKDIR=${RUNDIR}/wrk
+OUTFILE=${OUTFILE}
 
+cat<<EOF
+# ###########################################################
+# Announcement
+# -----------------------------------------------------------
+# Begin: $0
+EOF
 
 
 echo "# output the total size for the issues/PRs  in the flat file."
@@ -28,7 +39,9 @@ cat  ${RUNWRKDIR}/${OUTFILE}.txt  | grep 'SPRINT READY' | grep -v "^DRAFT" | cut
 
 echo "# List of the issues & PRs in the flat file along with their repo."
 
-tail -$(( $(wc -l < ${RUNWRKDIR}/${OUTFILE}.txt) -1 )) ${RUNWRKDIR}/${OUTFILE}.txt | grep 'SPRINT READY' | grep -v "^DRAFT" | cut -d'|' -f2,4,9 --output-delimiter='|' > ${RUNWRKDIR}/add_these.txt
+tail -$(( $(wc -l < ${RUNWRKDIR}/${OUTFILE}.txt) -1 )) ${RUNWRKDIR}/${OUTFILE}.txt | grep 'SPRINT READY' | grep -v "^DRAFT" | cut -d'|' -f3,4,6 --output-delimiter='|' > ${RUNWRKDIR}/add_these.txt
+
+
 
 # e.g.
 #'PULL_REQUEST'|8940|'dataverse'
